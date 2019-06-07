b357bff04da5053e91b9143f15a4d5c867aca7d7
Reese Lee <reese.lee.cy@gmail.com>
2019-06-07 12:02:32 -0700

    change location in URl to Portland's lat long  info

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ export class FindDoctor {
   findDrByMalady(malady) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5155,-122.6793,100&skip=2&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
