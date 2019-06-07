2259cf75c7ebc6e46ee70ab2787b5df616e5b0f2
Reese Lee <reese.lee.cy@gmail.com>
2019-06-07 10:08:50 -0700

    add promise function

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import { ClassName } from './class-name.js';
+import { FindDoctor } from './api.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  let className = new ClassName;
-  return className;
+  let findDoctor = new FindDoctor;
+  let promise = findDoctor.findDrByMalady(city);
+
+  promise.then(function(response) {
+    let body = JSON.parse(response);
+    $('.showMalady').text(`The doctors who treat ${malady} are: ${body.main.showDoctors}%`);
+    // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  });
 });
