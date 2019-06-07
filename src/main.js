c5f5ce2430cf54e20e57f3f96fc5e09bf9c53dcb
Reese Lee <reese.lee.cy@gmail.com>
2019-06-07 16:52:52 -0700

    add forEach for finding doctor info, and code for parsing in specific doctor info

--- src/main.js
+++ src/main.js
@@ -5,20 +5,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  $('#maladyType').click(function(){
+  $('form#search').submit(function(event) {
+    event.preventDefault();
     let name =  $('#name').val();
     $('#name').val("");
     let malady = $('#malady').val();
     $('#malady').val("");
     let findDoctor = new FindDoctor;
-    let promise = findDoctor.findDrByMalady(name, malady);
+    let promise = findDoctor.findDoctor(name, malady);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $('.showMalady').text(`The doctors who treat ${body.data.malady} are: ${body.data.name}`);
-      // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      if(body.data.length === 0) {
+        $('#showDoctors').text(`Time to move to Canada! There are no doctors in your area that match your search parameters.`)
+      }
+
+      body.data.forEach(function (data) {
+        if (data.profile.website === undefined){
+          data.profile.website = "No website"
+        }
+
+        $('#showDoctors').append(`${data.profile.first_name} ${data.profile.last_name}<br>
+
+          Specialty: ${data.specialties[0].description}<br>
+          Address: ${data.practices[0].visit_address.street}<br>
+          ${data.practices[0].visit_address.city}, ${data.practices[0].visit_address.state} ${data.practices[0].visit_address.zip}<br>
+          ${data.practices[0].phones[0].number}<br>
+          Website: ${data.profile.website}<br>
+          Accepting new patients: ${data.practices[0].accepts_new_patients}<br><hr><br>`)
+        }, function (error) {
+          $('#error').text(`There was an error processing your request: ${error.message}`)
+          //   $('#showDoctors').text(`The doctors who treat ${body.data.malady} are: ${body.data.name}`);
+          //   // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+          // }, function(error) {
+          // $('#errors').text(`There was an error processing your request: ${error.message}`);
+        });
+      });
+
     });
   });
-});
