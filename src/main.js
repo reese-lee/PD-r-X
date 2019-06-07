12a7a20f082b0796bb0f7141c9c4b2f0f987d5b2
Reese Lee <reese.lee.cy@gmail.com>
2019-06-07 14:28:40 -0700

    edit response

--- src/main.js
+++ src/main.js
@@ -8,16 +8,17 @@ $(document).ready(function() {
   $('#maladyType').click(function(){
     let name =  $('#name').val();
     $('#name').val("");
-
+    let malady = $('#malady').val();
+    $('#malady').val("");
     let findDoctor = new FindDoctor;
-    let promise = findDoctor.findDrByMalady(name);
+    let promise = findDoctor.findDrByMalady(name, malady);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $('.showMalady').text(`The doctors: ${body.data.name}`);
+      $('.showMalady').text(`The doctors who treat ${body.data.malady} are: ${body.data.name}`);
       // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
   });
-});
 });
