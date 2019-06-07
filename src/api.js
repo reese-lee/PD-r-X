0a89ba09df5a722adc1c108f811dc870d2d30dbd
Reese Lee <reese.lee.cy@gmail.com>
2019-06-07 14:27:20 -0700

    change url to include two more search fields, increase limit

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 export class FindDoctor {
 
-  findDrByMalady(malady) {
+  findDrByMalady(name, malady) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5155,-122.6793,100&skip=2&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${malady}&location=37.773%2C-122.413%2C100&sort=rating-asc&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
