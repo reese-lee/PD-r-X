ba7074bd8fbed77920113c5d3a2f2228ec0f4161
Reese Lee <reese.lee.cy@gmail.com>
2019-06-07 16:57:31 -0700

    change limit in URL to see if it is responsive

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ export class FindDoctor {
   findDoctor(name, malady) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${malady}&location=45.5155%2C-122.6793%2C10&sort=rating-asc&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${malady}&location=45.5155%2C-122.6793%2C10&sort=rating-asc&skip=0&limit=5&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
